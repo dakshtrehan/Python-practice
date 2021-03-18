bf4f50ed586dde0978163fddec45e8a5168bba24
Daksh Trehan <daksh.trehan@hotmail.com>
2021-03-18 17:15:24 +0530

    Find question inside the code

---
+++ Conditionals and Loops/number_to_digit.py
@@ -0,0 +1,25 @@
+#Write a program to input a digit and print it in words
+
+x=int(input("Enter the digit: "))
+if (x==1):
+    print("One")
+elif (x==2):
+    print("Two")
+elif (x==3):
+    print("Three")
+elif (x==4):
+    print("Four")
+elif (x==5):
+    print("Five")
+elif (x==6):
+    print("Six")
+elif (x==7):
+    print("Seven")
+elif (x==8):
+    print("Eight")
+elif (x==9):
+    print("Nine")
+elif (x==0):
+    print("Zero")
+else:
+    print("You have entered multi-digits")
